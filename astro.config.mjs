cb72d27dead60dc038683c77aeeae8f97c60209a
AndreiCagungun <andreicagungunf@gmail.com>
2025-05-30 10:50:22 +0800

    ngrok

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    allowedHosts: ['64a5-120-29-86-203.ngrok-free.app'],
+  },
+});
