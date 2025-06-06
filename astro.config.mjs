ad4c728e40d07f5d7e37c93ce6f2659473dc684f
AndreiCagungun <andreicagungunf@gmail.com>
2025-06-06 14:31:40 +0800

    name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   server: {
-    allowedHosts: ['64a5-120-29-86-203.ngrok-free.app'],
+    allowedHosts: ['4497-120-29-86-203.ngrok-free.app'],
   },
 });
